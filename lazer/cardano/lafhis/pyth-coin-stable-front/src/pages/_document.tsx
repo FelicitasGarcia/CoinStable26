fa33ebc22514af5ec94d0e0c3eb317f71d0128f1
Matias <matiasewaisman@gmail.com>
2026-03-22 20:05:07 -0300

    Cambie el readme y el icono que se sigue viendo mal

--- lazer/cardano/lafhis/pyth-coin-stable-front/src/pages/_document.tsx
+++ lazer/cardano/lafhis/pyth-coin-stable-front/src/pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
